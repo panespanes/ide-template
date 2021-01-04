ca53d3e5bcef46c3aa5b3d9ff96e956c26bbcb04
GitHub Action <action@github.com>
2021-01-04 03:28:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/panespanes/idetemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/panespanes/idetemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.panespanes.idetemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.panespanes.idetemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
